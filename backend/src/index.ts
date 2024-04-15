4e24527b89cf26e2e3e3cfd8345daaca2058cc25
himan1009 <shishusha922@gmail.com>
2024-04-15 23:52:09 +0530

    robust 3:00

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute"
+
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -11,9 +13,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) => {
-  res.json({ message: "HELLO !" });
-});
+// app.get("/test", async (req: Request, res: Response) => {
+//   res.json({ message: "HELLO !" });
+// });
+
+
+// /api/my/user
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
   console.log("server started on local host : 7000");
