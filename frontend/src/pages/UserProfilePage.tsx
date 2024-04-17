4bf1b834482e86e01afc2bd8f3dbcfc7a2f05c40
himan1009 <shishusha922@gmail.com>
2024-04-17 11:25:38 +0530

    robust 4:30

---
+++ frontend/src/pages/UserProfilePage.tsx
@@ -0,0 +1,10 @@
+// import { useGetMyUser, useUpdateMyUser } from "@/api/MyUserApi";
+import { useUpdateMyUser } from "@/api/MyUserApi";
+import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
+
+const UserProfilePage = () => {
+  const { updateUser, isLoading } = useUpdateMyUser();
+  return <UserProfileForm onSave={updateUser} isLoading={isLoading} />;
+};
+
+export default UserProfilePage;
